c7babd23e9c74ddb5e8fe61ae79f7ed1f48acde3
Natalia <natalia-321s@outlook.com>
2020-11-19 23:53:37 -0300

    Crewmate

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <AmongUs/>
+  </div>
+</template>
+
+<script>
+import AmongUs from './components/AmongUs.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AmongUs
+  }
+}
+</script>
+
+<style>
+*, *:before, *:after{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+</style>
